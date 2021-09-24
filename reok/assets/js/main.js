e8f073b9256a8656685bd37362f9b653d666f91f
Jofay-zs <jofayzs19@gmail.com>
2021-09-24 16:05:08 -0500

    Subiendo otras funcionalidades para la barra de navegacion y subiendo el footer reutilizable

--- reok/assets/js/main.js
+++ reok/assets/js/main.js
@@ -1,4 +1,9 @@
+/**
+ * Color change for the navbar
+ * 
+ * Change the color of the navbar whe you scroll down
+ */
 window.addEventListener("scroll", function(){
     var navbar = document.querySelector("nav");
     navbar.classList.toggle("sticky", window.scrollY > 0);
-})
+});
