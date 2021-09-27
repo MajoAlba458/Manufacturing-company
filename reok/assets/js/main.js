7bf82a03d085a7127c06a1b5477adf5ee61697ab
Jofay-zs <jofayzs19@gmail.com>
2021-09-27 09:56:53 -0500

    Subiendo cambios de colores para la barra de navegacion, subiendo el footer y subiendo el login con los nuevos campos requeridos

--- reok/assets/js/main.js
+++ reok/assets/js/main.js
@@ -7,3 +7,28 @@ window.addEventListener("scroll", function(){
     var navbar = document.querySelector("nav");
     navbar.classList.toggle("sticky", window.scrollY > 0);
 });
+
+/**
+ * Popup 
+ * 
+ * A popup for the signup and forgot password section
+ */
+
+let login = document.getElementById('login-container');
+let signup = document.getElementById('signup-container');
+let forgot = document.getElementById('forgot-container');
+
+function toggleLoginSignup(){
+    login.classList.toggle("active");
+    signup.classList.toggle("active");
+}
+
+function toggleLoginForgot(){
+    login.classList.toggle("active");
+    forgot.classList.toggle("active");
+}
+
+function toggleSignupForgot(){
+    signup.classList.toggle("active");
+    forgot.classList.toggle("active");
+}
