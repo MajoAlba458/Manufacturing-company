3bc5c9950f5d1472c721f5dfe9652eed5a0b90b7
Jofay-zs <jofayzs19@gmail.com>
2021-09-30 21:26:51 -0500

    Agregando botones alternativos para el login

--- reok/src/components/login/LoginForm.jsx
+++ reok/src/components/login/LoginForm.jsx
@@ -4,7 +4,7 @@ import LeftContainerLoginForm from './LeftContainerLoginForm';
 
 const LoginForm = () => {
   return (
-    <div class="form-container" id="login-container">
+    <div className="form-container" id="login-container">
       <LeftContainerLoginForm image={LoginImage} />
       <HeaderLoginForm />
       <RightContainerLoginForm />
@@ -14,33 +14,51 @@ const LoginForm = () => {
 
 const RightContainerLoginForm = () => {
   return (
-    <div class="row form-container__right">
-      <form action="../index.html" class="d-grid gap-2">
-        <h2 class="text-center">Login</h2>
+    <div className="row form-container__right">
+      <form action="../index.html" className="d-grid gap-2">
+        <h2 className="text-center">Login</h2>
         <p>
           If you already have an account enter your email and enjoy our app.
         </p>
-        <div class="input-box">
-          <div class="input-box__input">
-            <i class="fas fa-envelope"></i>
-            <label for="email">Email</label>
-            <input type="email" class="email" name="email" required="true" />
+        <div className="input-box">
+          <div className="input-box__input">
+            <i className="fas fa-user"></i>
+            <label for="user">Username</label>
+            <input type="text" className="user" name="user" required="true" />
+          </div>
+        </div>
+        <div className="input-box">
+          <div className="input-box__input">
+            <i className="fas fa-lock"></i>
+            <label for="email">Password</label>
+            <input type="password" className="password" name="password" required="true" />
+          </div>
+        </div>
+        <div className='otherLinksToLogin'>
+          <div className='btn btn-primary fs-2'>
+            <i className="fab fa-google-plus-g"></i>
+          </div>
+          <div className='btn btn-primary fs-2'>
+            <i className="fab fa-facebook-f"></i>
+          </div>
+          <div className='btn btn-primary fs-2'>
+            <i className="fab fa-apple"></i>
           </div>
         </div>
         <input
           type="submit"
-          class="btn btn-primary btn-submit"
+          className="btn btn-primary btn-submit"
           value="Login"
           id="input-login"
         />
-        <div class="backup-links">
-          <div class="text-end">
-            <button onclick="toggleLoginSignup()" type="button" class="btn">
+        <div className="backup-links">
+          <div className="text-end">
+            <button onclick="toggleLoginSignup()" type="button" className="btn">
               Signup
             </button>
           </div>
-          <div class="text-end">
-            <button onclick="toggleLoginForgot()" type="button" class="btn">
+          <div className="text-end">
+            <button onclick="toggleLoginForgot()" type="button" className="btn">
               Forgot password
             </button>
           </div>
